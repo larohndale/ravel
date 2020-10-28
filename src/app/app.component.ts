7898ba3bd2dbd6f2b62b1b2b89466e3e61bb2d01
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-10-28 12:29:25 +0530

    Minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'ravel';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {}
 }
