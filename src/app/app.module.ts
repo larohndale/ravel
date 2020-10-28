4e931be096bdda55a4c1003d086a5dc585b7d8d9
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-10-28 12:18:32 +0530

    Added components & user service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routes/home/home.component';
+import { CodeComponent } from './components/blog/code/code.component';
+import { ContentComponent } from './components/blog/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CodeComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
