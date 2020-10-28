ed5d4cbc7d0a584861b5228bba080e2b470ec2e8
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-10-28 12:27:00 +0530

    Added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './routes/home/home.component';
-import { CodeComponent } from './components/blog/code/code.component';
-import { ContentComponent } from './components/blog/content/content.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./routes/home/home.component";
+import { CodeComponent } from "./components/blog/code/code.component";
+import { ContentComponent } from "./components/blog/content/content.component";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { environment } from "src/environments/environment.prod";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    CodeComponent,
-    ContentComponent
-  ],
+  declarations: [AppComponent, HomeComponent, CodeComponent, ContentComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAakaOGzV9sZh4UdKBCHkAW8aOmcAOpus8',
+    authDomain: 'ravel-4b2e2.firebaseapp.com',
+    databaseURL: 'https://ravel-4b2e2.firebaseio.com',
+    projectId: 'ravel-4b2e2',
+    storageBucket: 'ravel-4b2e2.appspot.com',
+    messagingSenderId: '1049357969802',
+    appId: '1:1049357969802:web:7378524894041771860073',
+    measurementId: 'G-72K4XZ76QP',
+  },
 };
