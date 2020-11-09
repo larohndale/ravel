81e42fbc9e7f66f1a70ebfdc23aa3078d4bad9c2
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-09 21:00:36 +0530

    Added ArticleInterface

--- src/app/services/article/interfaces.ts
+++ src/app/services/article/interfaces.ts
@@ -1,3 +1,11 @@
 export interface ArticleInterface {
   id: string;
+  author: string;
+  title: string;
+  tags: string[];
+  content: string;
+  upvoters: string[];
+  downvoters: string[];
+  datePublished: Date;
+  dateEdited: Date[];
 }
